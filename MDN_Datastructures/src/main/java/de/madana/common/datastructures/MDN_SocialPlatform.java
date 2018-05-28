69752422fedb7238a556175058fedaa414fe7e29
J.-Fabian Wenisch <wenisch@madana.io>
2018-05-28 18:56:52 +0200

    DEV-73 #time 30m #comment Created Handler which returns all Profiles of MADANA on Social Platforms, based on the data in the database

---
+++ MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_SocialPlatform.java
@@ -0,0 +1,32 @@
+package de.madana.common.datastructures;
+
+public class MDN_SocialPlatform
+{
+	private String name= "";
+	private String link="";
+	private String icon="";
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
