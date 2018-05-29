4fbb06f8e8e546c34a2bd341385858b6ad5c93fc
J.-Fabian Wenisch <wenisch@madana.io>
2018-05-29 17:13:59 +0200

    DEV-241 #time 1h #comment The created Token is now stored in an object and send as JSON

---
+++ MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_Token.java
@@ -0,0 +1,20 @@
+package de.madana.common.datastructures;
+/**
+ * 
+ * @author J.-Fabian Wenisch
+ *
+ */
+public class MDN_Token 
+{
+	public String getToken() 
+	{
+		return token;
+	}
+
+	public void setToken(String token) 
+	{
+		this.token = token;
+	}
+
+	String token;
+}
