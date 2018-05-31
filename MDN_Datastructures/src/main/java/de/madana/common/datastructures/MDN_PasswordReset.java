310bff587835aa814c4e4eaddf011835b700c5f3
J.-Fabian Wenisch <wenisch@madana.io>
2018-05-31 19:45:49 +0200

    DEV-253 #time 2h #comment Implemented PasswordRecovery / PasswordReset Endpoint. @Post on password requires an registered mail address and sends out a token via the smtpclient. The token can be used together with the mailaddress and a new password in the @Put functionality to reset the password. The requested token is valid for 1hour and will be discarded afterwards. Therefore a Watchdog ( TimerTask) is implemented which runs every 5 minute and removes not valid tokens.

---
+++ MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_PasswordReset.java
@@ -0,0 +1,27 @@
+package de.madana.common.datastructures;
+
+public class MDN_PasswordReset
+{
+private String password;
+private  String mail;
+private  String token;
+
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getMail() {
+	return mail;
+}
+public void setMail(String mail) {
+	this.mail = mail;
+}
+public String getToken() {
+	return token;
+}
+public void setToken(String token) {
+	this.token = token;
+}
+}
