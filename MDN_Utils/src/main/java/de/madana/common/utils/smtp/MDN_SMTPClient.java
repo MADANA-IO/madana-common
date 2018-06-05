94feeaa22726a3dabc7799c7c3f64f1659aee4c8
J.-Fabian Wenisch <wenisch@madana.io>
2018-06-05 11:41:43 +0200

    DEV-253 #time 15m #comment When a new password has been set with the password reset the token is automatically removed and the server doesn't wait for the cleanup watchdog

--- MDN_Utils/src/main/java/de/madana/common/utils/smtp/MDN_SMTPClient.java
+++ MDN_Utils/src/main/java/de/madana/common/utils/smtp/MDN_SMTPClient.java
@@ -80,23 +80,5 @@ public class MDN_SMTPClient
 				"This message may contain confidential and/or privileged information. If you have received this e-mail in error please notify the sender immediately and delete the material from any computer. <br>\r\n" + 
 				"Our company accepts no liability for the content of this email, or for the consequences of any actions taken on the basis of the information provided, unless that information is subsequently confirmed in writing. For discussion purposes only and cannot be used to create a binding contract.";
 	}
-	public static void main(String[] args) 
-	{
-		MDN_SMTPClient oClient = new MDN_SMTPClient("noreply@community.madana.io", "!ndkölashd7üUJIKlködssöJ" 
-				, "smtp.1und1.de", "noreply@community.madana.io", "587", true, true);
-		
-		try {
-			String strMailText ="<div style =\"bgcolor=#f3f3f6;\">			<div>\r\n" + 
-					"                <img src=\"http://www.madana.io/images/logo.png\"\r\n" + 
-					"					alt=\"MADANA Logo\"\r\n" + 
-					"					width=\"100\" height=\"100\" style=\"text-align:center; \r\n" + 
-					"display:block;\"></div><h1>Password reset requested</h1><br> <br> To change the password for your MADANA account, please click on the following link.<br><br>LINK <br><br>"
-					+ "This link will become invalid 6 hours after submission. The link will take you to the MADANA account management website, where you can enter a new password.<br>"
-					+ "This e-mail was sent automatically. Please do not reply to this mail.</div>";
-			oClient.sendMail("TEST", "wenisch@madana.io", strMailText);
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+
 }
