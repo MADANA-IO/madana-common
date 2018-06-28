985a33af83e7eb74a87d54cb329ccf1336654610
J.-Fabian Wenisch <wenisch@madana.io>
2018-06-28 18:27:16 +0200

    DEV-307 #time 3h #comment Webclient redirects the user to the Twitter Authentication page and accepts the Twitter callback and receives the ouauthtoken and verifier

---
+++ MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_OAauthToken.java
@@ -0,0 +1,28 @@
+package de.madana.common.datastructures;
+
+public class MDN_OAauthToken 
+{
+	String token;
+	String verifier;
+	public String getToken() 
+	{
+		return token;
+	}
+
+	public void setToken(String token) 
+	{
+		this.token = token;
+	}
+
+	public String getVerifier() 
+	{
+		return verifier;
+	}
+
+	public void setVerifier(String verifier)
+	{
+		this.verifier = verifier;
+	}
+
+	
+}
