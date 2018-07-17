fd93b6b40aa7b0a9409d36c3173d2487442de745
J.-Fabian Wenisch <wenisch@madana.io>
2018-07-17 18:48:14 +0200

    DEV-414 #time 15m #comment user image is embedded in user object and userprofile

--- MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_A_UserObject.java
+++ MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_A_UserObject.java
@@ -5,7 +5,14 @@ public abstract class MDN_A_UserObject
 	String userName="";
 	String created="";
 	String lastActive="";
+	String image="";
 
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
 	public String getUserName() {
 		return userName;
 	}
