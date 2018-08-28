c0f8165efc778d425d03098bee16db02c893ab8b
J.-Fabian Wenisch <wenisch@madana.io>
2018-08-28 18:26:45 +0200

    DEV-524 #time 3m #comment added missing responsetoken class

---
+++ MDN_Datastructures/src/main/java/de/madana/common/datastructures/MDN_TwitterResponseToken.java
@@ -0,0 +1,14 @@
+package de.madana.common.datastructures;
+
+public class MDN_TwitterResponseToken 
+{
+String response_token;
+
+public String getResponse_token() {
+	return response_token;
+}
+
+public void setResponse_token(String response_token) {
+	this.response_token = response_token;
+}
+}
